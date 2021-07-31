17d9e67e70717eaf8f3c089ad9ea04a8dcc3d0e8
Giovanni Orozco <whoisyio@gmail.com>
2021-07-31 09:06:48 -0600

    Enter

--- PRUEBA_PROGRAMACION/EJERCICIO 1/JS/ejercicio1.js
+++ PRUEBA_PROGRAMACION/EJERCICIO 1/JS/ejercicio1.js
@@ -1,30 +1,36 @@
-'use strict'
-window.addEventListener('load',function(){
-    
-    
-    var array=this.document.querySelector("#array");
-    const Parbalanceado=function(par){
-        const stack=[];
-        for(let single of par){
-            if(single=='('){
-                stack.push(single);
-            }
-            if(single==')'){
-                if(stack.length==0){
-                    return false;
-                }
-                stack.pop();
-            }
-        }
-        return stack.length===0;
-    }
-    var resultado=this.prompt("Ingrese el dato a validar",'()');
-    let validcion=Parbalanceado(resultado);
-    if(validcion==false){
-        array.innerHTML="Par ingresado incorrecto";
-    }else{
-        array.innerHTML="Par ingresado correcto";
-    }
-   
+// De una cadena de texto de n longitud, 
+// diseñar un programa que obtenga el 
+// numero de veces que se repite una palabra en el texto
+
+const texto = '[(]]'
+
+let corchetes_1 = 0;
+let corchetes_2 = 0;
+
+let parentesis_1 = 0;
+let parentesis_2 = 0;
+
+let llaves_1 = 0;
+let llaves_2 = 0;
+
+let otros = 0
+
+for (let c of texto) {
+    if (c === '[') {
+        corchetes_1 += 1;
+    } else if (c === ']') {
+        corchetes_2 += 1;
+    } else if (c === '(') {
+        parentesis_1 += 1;
+    } else if (c === ')') {
+        parentesis_2 += 1;
+    } else if (c === '{') {
+        llaves_1 += 1;
+    } else if (c === '}') {
+        llaves_2 += 1;
+    } else otros += 1;
 }
-)
+
+if (corchetes_1 != corchetes_2 || parentesis_1 != parentesis_2 || llaves_1 != llaves_2) {
+    console.log('Incorrecto');
+} else console.log('Correcto');
